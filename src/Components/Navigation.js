64a6ad7053f80f07b7f8b005ae864ab29787b7b2
Liam <helm_of_protection@hotmail.com>
2023-03-23 01:39:17 -0800

    modified icon

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
 import '../App.css';
+import github from '../images/github.svg'
 
 function Navigation({onRouteChange, isSignedIn}) {
     if (isSignedIn) {
         return(
             <nav>
-                <a href="https://github.com/liam2258/FaceRecognition"><img src={require('../images/github2.png')} alt='Face-Detection' width={'40px'} height={'40px'} padding={'13px'}></img></a>
+                <a href="https://github.com/liam2258/FaceRecognition"><img src={github} alt='Face-Detection' width={'40px'} height={'40px'} padding={'13px'}></img></a>
                 <div style={{display: 'flex', padding: '10px'}}>
                     <button onClick={() => onRouteChange('signout')}>Sign Out</button>
                 </div>
@@ -14,7 +15,7 @@ function Navigation({onRouteChange, isSignedIn}) {
     } else {
         return(
             <nav>
-                <a href="https://github.com/liam2258/FaceRecognition"><img src={require('../images/github2.png')} alt='Face-Detection' width={'40px'} height={'40px'} padding={'13px'}></img></a>
+                <a href="https://github.com/liam2258/FaceRecognition"><img src={github} alt='Face-Detection' width={'40px'} height={'40px'} padding={'13px'}></img></a>
                 <div style={{display: 'flex', gap: '10px', padding: '10px'}}>
                     <button style={{cursor: 'pointer'}} onClick={() => onRouteChange('signin')}>Sign In</button>
                     <button style={{cursor: 'pointer'}} onClick={() => onRouteChange('register')}>Register</button>
